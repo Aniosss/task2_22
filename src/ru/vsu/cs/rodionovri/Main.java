1ebcd15e3a9b747fe426cdcafa179c494c08d368
Aniosss <rom.rodionov48@gmail.com>
2022-09-11 22:32:56 +0300

    2task

--- src/ru/vsu/cs/rodionovri/Main.java
+++ src/ru/vsu/cs/rodionovri/Main.java
@@ -3,46 +3,49 @@ package ru.vsu.cs.rodionovri;
 import java.util.*;
 
 public class Main {
-    public static int input() {
+    public static void inputSizeOfBox(int array[]) {
         Scanner scanner = new Scanner(System.in);
 
-        return scanner.nextInt();
-    }
-
-    public static void output(int var) {
-        if (var == 0) {
-            System.out.println("equals");
-        } else if (var == 1) {
-            System.out.println("first is bigger");
-        } else if (var == 2) {
-            System.out.println("second is bigger");
-        } else {
-            System.out.println("no");
+        for (int i = 0; i < 3; ++i) {
+            array[i] = scanner.nextInt();
         }
     }
 
-    public static int compare(int first[], int second[]) {
-        Arrays.sort(first);
-        Arrays.sort(second);
-        if (first[0] == second[0] && first[1] == second[1] && first[2] == second[2]) {
+    public static void output(int var) {
+        switch (var) {
+            case 0:
+                System.out.println("equals");
+                break;
+            case 1:
+                System.out.println("first is bigger");
+                break;
+            case 2:
+                System.out.println("second is bigger");
+                break;
+            default:
+                System.out.println("no");
+                break;
+        }
+    }
+
+    public static int compareTwoBoxes(int firstBox[], int secondBox[]) {
+        Arrays.sort(firstBox);
+        Arrays.sort(secondBox);
+        if (firstBox[0] == secondBox[0] && firstBox[1] == secondBox[1] && firstBox[2] == secondBox[2]) {
             return 0;
-        } else if (first[0] >= second[0] && first[1] >= second[1] && first[2] >= second[2]) {
+        } else if (firstBox[0] >= secondBox[0] && firstBox[1] >= secondBox[1] && firstBox[2] >= secondBox[2]) {
             return 1;
-        } else if (first[0] <= second[0] && first[1] <= second[1] && first[2] <= second[2]) {
+        } else if (firstBox[0] <= secondBox[0] && firstBox[1] <= secondBox[1] && firstBox[2] <= secondBox[2]) {
             return 2;
         } else return 3;
     }
 
 
     public static void main(String[] args) {
-        int first[] = new int[3];
-        int second[] = new int[3];
-        for (int i = 0; i < 3; ++i) {
-            first[i] = input();
-        }
-        for (int i = 0; i < 3; ++i) {
-            second[i] = input();
-        }
-        output(compare(first, second));
+        int firstBox[] = new int[3];
+        int secondBox[] = new int[3];
+        inputSizeOfBox(firstBox);
+        inputSizeOfBox(secondBox);
+        output(compareTwoBoxes(firstBox, secondBox));
     }
 }
